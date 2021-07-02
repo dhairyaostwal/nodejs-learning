e8475f7106beab58b2e9c1d9013a765966e1a0b6
Dhairya <dhairya.ostwal@gmail.com>
2021-07-02 19:43:24 +0530

    Added GET/PUSH

--- Natours-API/app.js
+++ Natours-API/app.js
@@ -1,15 +1,50 @@
 const express = require('express')
+const fs = require('fs')
+const bodyParser = require('body-parser')
 const app = express()
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-    res
-        .status(404)
-        .json({ message: "Hey", app: "Natours" });
-    console.log("Getting from / route")
+const tours = JSON.parse(
+    fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+const reviews = JSON.parse(
+    fs.readFileSync('./dev-data/data/reviews.json')
+);
+
+app.get('/api/v1/tours', (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        result: tours.length,
+        data: {
+            tours: tours
+        }
+    })
 })
 
-app.post('/', (req, res) => {
-    res.send("Posting to the URL /")
+app.post('/api/v1/tours', (req, res) => {
+    // console.log(req.body);
+    const newID = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({ id: newID }, req.body);
+    tours.push(newTour);
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+        res.status(201).json({
+            status: 'success',
+            data: {
+                tour: newTour
+            }
+        });
+    })
+})
+
+app.get('/api/v1/reviews', (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        result: reviews.length,
+        data: {
+            reviews: reviews
+        }
+    })
 })
 
 app.listen(3000, () => {
